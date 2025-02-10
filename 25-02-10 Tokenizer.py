132d82d095b4a5dccaacfbcca3478c03253dd5b5
Alejo <alonsoalejo11@gmail.com>
2025-02-10 09:06:39 -0500

    Archivo de ejemplo de modelo preentrenado

--- 25-02-10 Tokenizer.py
+++ 25-02-10 Tokenizer.py
@@ -1,4 +1,12 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+# Cargar un modelo preentrenado
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+model = AutoModelForCausalLM.from_pretrained("gpt2")
 
-print("helloworld!")
+text = "hola soy juan y hoy vo a ir"
+
+inputs = tokenizer(text, return_tensors="pt")
+
+# mostrar los tokens como números
+print(input["imputs_ids"])
